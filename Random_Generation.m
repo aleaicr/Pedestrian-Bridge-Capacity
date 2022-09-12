51daa9482efe4284a933e9f9c91e70b48d49bef6
Alexis Contreras R <ale.aicr@gmail.com>
2022-09-12 00:34:41 -0300

    Update Random_Generation.m

--- Random_Generation.m
+++ Random_Generation.m
@@ -8,11 +8,13 @@ close all
 clc
 
 %% Cantidad máxima de peatones
-n_max = 100;                                                                 % Cantidad máxima de peatones a considerar
-n_vect = (1:1:n_max)';
+n_min = 1;
+n_max = 10;
+n_step = 1;
+peatones = (n_min:n_step:n_max)';
 %% Masa
-mu_m = 70;  % kg                                                            % Media de la distribución de masa
-sigma_m = 3; % kg                                                           % Desviación estándar de la distribución de masa
+mu_m = 80;  % kg                                                            % Media de la distribución de masa
+sigma_m = 5; % kg                                                           % Desviación estándar de la distribución de masa
 m_vect = normrnd(mu_m,sigma_m,[n_max,1]);                                       % Valores aleatorios de masa para 'n' peatones
 
 %% Velocidad
@@ -23,7 +25,7 @@ v_vect = normrnd(mu_v,sigma_v,[n_max,1]);                                       
 %% Frecuencia caminar
 mu_freq = 1.8; % hz                                                         % Media                                                       
 sigma_freq = 0.5;  % hz                                                     % Desviación estándar
-freq_vect = normrnd(mu_freq,sigma_freq,[n_max,1]);                              % Vector
+freq_vect = normrnd(mu_freq,sigma_freq,[n_max,1]);   % rad/sec                           % Vector
 
 %% Tiempo para añadir
 % Tiempo que se demora para añadir al peatón i
@@ -43,6 +45,7 @@ tabla.Tadd = Tadd_vect;
 tabla.lado = side;
 tabla.Masa = m_vect;
 tabla.Velocidad = v_vect;
+tabla.Frecuencia = freq_vect;
 disp(tabla)                                                                 % Se muestra tabla
 clear tabla
 
@@ -56,6 +59,17 @@ histogram(freq_vect,'Normalization','pdf');
 histogram(Tadd_vect,'Normalization','pdf');
 hold off
 legend('Masa','Velocidad','Frecuencia','Tadd')
+
+%% Figura Peatones en puente vs tiempo
+
+Tadd_cum = cumsum(Tadd_vect);
+figure
+stairs(Tadd_cum,peatones)
+ylim([0,n_max])
+grid on
+xlabel('tiempo (t) [sec]')
+ylabel('Cantidad de peatones en puente')
+title('Cantidad de peatones simultáneamente en el puente')
 
 %% Eliminar variables que ya no me sirven
 % O se podría definir la misma variable mu y sigma en cada parámetro y solo
