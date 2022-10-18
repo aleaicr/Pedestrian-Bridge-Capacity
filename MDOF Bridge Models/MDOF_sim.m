bc8f5a19391ae58275f745bf38de146ade7e5417
Alexis Contreras R <ale.aicr@gmail.com>
2022-10-17 21:01:59 -0300

    Update
    SImplificar con funciones
    - assumedModes_beam()
    - SpaceState_bridge()
    - sinModalShapes()

--- MDOF Bridge Models/MDOF_sim.m
+++ MDOF Bridge Models/MDOF_sim.m
@@ -47,13 +47,6 @@ x_vals = 0:L/cant_particiones:L;                                            % Di
 % Fuerzas peatonales
 P = Po*sin(Omega*t);                                                        % Carga horizontal distribuida en el puente
 
-% Notar que no depende de x, ya que estas asumen que todas las personas
-% están sincronizadas en todo el largo del puente. Para este modelo de puente,
-% no es necesario que las personas estén sincronizadas, se pueden reingresar 
-% al puente en un simulink o definir una función en el script de matlab que
-% depende de x. Si se quieren incluir individualmente, entonces Pe tendría
-% que componerse por una sumatoria de fuerzas puntuales.
-
 % Parámetros simulaciones
 t_init = 0;                                                                 % Tiempo inicial de la simulación
 t_final = 30;                                                               % Tiempo final de la simulación
@@ -65,11 +58,8 @@ t_length = length(t_vect);
 %% Modos asumidos
 % psi_n = sin(n*pi*x/L)
 
-% psis = struct();                                                            % Inicializar struct     
 psi = sym(zeros(1,cant_modos));
 for n = 1:cant_modos
-%     nameVal = strcat('psi',string(n));                                      % Nombre de variable
-%     psis.(nameVal) = sin(n*pi*x/L);                                         % Forma modal de cada modo
     psi(n) = sin(n*pi*x/L);
 end
 
@@ -86,115 +76,20 @@ end
 dpsi = diff(psi,x);
 ddpsi = diff(dpsi,x);
 
-% Graficar 
-% Para ver que son cinemáticamente admisibles
 
-% Condición 1: cumplir con las condiciones de borde geométricas.
-% Condición 2: Funciones continuas y suaves.
-% Graficar los modos asumidos, psi
-% figure
-% hold on
-% fplot(psi1,[0 L])
-% fplot(psi2,[0 L])
-% hold off
-% xlabel('x'), ylabel('\psi (x)'), legend('\psi_1','\psi_2')
-% grid on
-
-% Condición 3: Debe poseer derivadas de orden mayor. 
-% Graficar derivadas espaciales, dpsi
-% figure
-% hold on
-% fplot(dpsi(1),[0 L])
-% fplot(dpsi(2),[0 L])
-% hold off
-% xlabel('x'), ylabel('$\dot{\psi} (x)$','Interpreter','latex'), legend('$\dot{\psi_1}$','$\dot{\psi_2}$','Interpreter','latex')
-% grid on
-
-% Condición 4: Deben formar una base.
-% Test de ortogonalidad
-% test = int(psi1*psi2,x,0,L);    
-
-%% EDM
-% Me*ddq(t) + Ke*q(t) = Pe
-
-%% Matriz de masa equivalente
-% Me = Mviga (cinética ¿?)
-Me = double(int(rho_lin*A*(psi).'*psi,x,0,L));
-
-%% Matriz de rigidez equivalente
-% Por energía potencial de deformación del puente
-Ke = double(int(E*I*(ddpsi).'*ddpsi,x,0,L));
-
-%% Fuerza externa equivalente 
-% Fuerza peatonal (sinusoide) igual en todo el largo representa que todas
-% las personas están sincronizadas
-Pe = int(P*psi.',x,0,L);                                                     % Fuerza externa equivalente 
-Ge = eye(cant_modos,cant_modos);                                             % Matriz de influencia de Pe (exictación), 2x2
-
-%% Matriz de amortiguamiento equivalente
-% Energí­a potencial de deformación del puente (viga equivalente)
-
-% Ce = double(int(xi*(psi)'*psi,x,0,L)); 
-% Esta fórmula es para amortiguadores físicos, no es proporcional ni 
-% inherente a la estructura necesariamente
- 
-% % Amortiguamiento de Rayleigh (No ocupar)
-% [Phi, lambda] = eig(Ke,Me);                                                 % Problema de valores y vectores propios
-% wr = diag(lambda.^0.5);                                                     % Frecuencia de cada modo                                                           % Periodo de cada modo
-% Mr = Phi'*Me*Phi;
-% Kr = Phi'*Ke*Phi;
-% 
-% % Utilizando solo los dos primeros modos
-% syms alfa beta
-% sol = solve(zrmodal(1) - 1/2*(alfa/wr(1) + beta*wr(1)),zrmodal(2) - 1/2*(alfa/wr(2) + beta*wr(2)));
-% alfa = double(sol.alfa);
-% beta = double(sol.beta);
-% clear sol
-% % Componer Ce
-% Cr = alfa*Mr + beta*Kr;                                                     % alfa es mucho mayor que beta, mucha participación de la masa (cuidado con sobreestimar)
-% Ce = alfa*Me + beta*Ke;
-
-% Amortiguamiento proporcional modal
-% zrmodal: tiene todos los amortiguamientos que se quieren para cada modo
-[~, lambda] = eig(Ke,Me);                                                   % Problema de valores y vectores propios
-wn = sqrt(diag(lambda));  
-Phi = eye(cant_modos);
-Ce = (Me*Phi)*diag(2*zrmodal.*wn./Me).*(Me*Phi).';    % Esto es C
-
-% Cmododal = Phi'*C*Phi  (segunda forma)                           % Esto es Cr
-[Phi, lambda] = eig(Ke,Me);                                                 % Problema de valores y vectores propios
-Ce2 = (Me*Phi)*diag(2*zrmodal.*wn./Me).*(Me*Phi).';    % Esto es C
-
-%% Participación modal
-% gammae = diag(Me^-1*Ge);    % No está listo, o malo
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%% OBTENER PARTICIPACIÓN MODAL DE CADA MODO %%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% EDM Modos Asumidos
+% Me*ddq(t) + Ce*dq(t) Ke*q(t) = Ge*Pe
+[Me,Ke,Ce,Pe,Ge,wn] = assumedModes_beam(psi,E,I,rho,A,L,zrmodal,P);
 
 %% Espacio Estado - Puente 
 % Respuesta del sistema dinámico uilizando Integracion Directa 
-
-% Ecuacion de Estado 
-% dx(t) = As*x(t) + Bs*u(t), donde x(t) = [q1(t) q2(t) dq1(t) dq2(t)] y u(t) = Pe(t) 
-As = [zeros(cant_modos) eye(cant_modos); -Me\Ke -Me\Ce];                    % Matriz de estados, 4x4
-Bs = [zeros(cant_modos); Me\Ge];                                             % Matriz de influencia de excitacion pe1(t), 4x1
-
-% Ecuacion de Respuesta 
-% y = Cs*x + Ds*p(t), donde y = [q1(t); q2(t); dq1; dq2(t);  ddq1(t); ddq2(t)]
-Cs = [eye(cant_modos) zeros(cant_modos) ; zeros(cant_modos) eye(cant_modos); -Me\Ke -Me\Ce]; % Matriz de estados, 6x4
-Ds = [zeros(cant_modos) ; zeros(cant_modos); Me\Ge];                         % Matriz de influencia de excitacion pe1(t), 6x1
+[modelo,As,Bs,Cs,Ds] = SpaceState_bridge(cant_modos,Me,Ke,Ce,Ge);
 
 % Condiciones Iniciales
 x0 = 0;                                                                     % Reposo
 
-% M
-modelo = ss(As,Bs,Cs,Ds);
-% bode(modelo)
-% eig(modelo_sin_tmd)
+% Mostrar propiedades del modelo
 damp(modelo)
-% modelo
 
 %% Simulación puente
 % Simulación del puente para la carga equivalente peatonal sinusoidal Pe
@@ -203,8 +98,8 @@ damp(modelo)
 % Fijarse en que tienen los valores de tiempo, Omega, L coherentes con los
 % que están en archivo Pe_vect_generator.m
 
-Pe_vect = load(Pe_vect_data.mat);
-Pe_vect_sim = [t_vect Pe_vect.Pe_vect];
+Pe_vect = [];
+Pe_vect_sim = [t_vect Pe_vect];
 % id est [0 Pe1 Pe2; 0.1 Pe1 Pe2; 0.2....];
 
 % Ejecutar simulación
--- MDOF Bridge Models/assumedModes_beam.m
+++ MDOF Bridge Models/assumedModes_beam.m
@@ -1,14 +1,40 @@
-function [Me,Ke,Ce,Pe] = assumedModes_beam(psi,E,I,rho,A,L,zrmodal,P)
+function [Me,Ke,Ce,Pe,Ge,wn] = assumedModes_beam(psi,E,I,rho,A,L,zrmodal,P)
+% Alexis Contreras R.
+% Pedestrian Bridge Capacity
+
 % Genera matrices de masa, rigidez y amortiguamiento utilizando el método
-% de modos asumidos de una viga.
+% de MODOS ASUMIDOS de una viga.
 
-
-%% Inputs
+%%
+% Inputs
 % psi           -> (sym) Vector de funciones con las formas modales
 % E             -> Modulo de elasticidad de la viga equivalente
 % I             -> Inercia en dirección de análisis de la viga equivalente
 % rho           -> Densidad lineal de la viga equivalente
-% A             -> 
+% A             -> Area de sección de la viga equivalente
+% L             -> Largo de la viga
+% zrmodal       -> Amortiguamiento de cada modo
+% P             -> Función de carga (integrable)
+
+% Outputs
+% Me            -> Matriz equivalente
+% Ke            -> Rigidez equivalente
+% Ce            -> Amortiguaiento equivalente (proporcional)
+% Pe            -> Carga equivalente
+% Ge            -> Participación equivalente (en verdad queda como una
+% identidad siempre) (no es la participación modal, aun no la saco)
+
+%%
+Me = double(int(rho*A*(psi).'*psi,x,0,L));
+Ke = double(int(E*I*(ddpsi).'*ddpsi,x,0,L));
+Pe = int(P*psi.',x,0,L);  
+Ge = eye(cant_modos,cant_modos); 
+
+% Amortiguamiento modal proporcional
+[~, lambda] = eig(Ke,Me);                                                   % Problema de valores y vectores propios
+wn = sqrt(diag(lambda));  
+Phi = eye(cant_modos);
+Ce = (Me*Phi)*diag(2*zrmodal.*wn./Me).*(Me*Phi).';    % Esto es C
 
 end
 
